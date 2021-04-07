bf62fc742cc1770f38d8ee1bbf55622aee99fe0e
sindhurasurapaneni <sindhura.kp114@gmail.com>
2021-04-06 23:33:16 -0400

    PyPoll Code update

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -21,13 +21,20 @@ with open(election_csv, newline='') as csvfile:
             Khan = Khan+1
         elif row[2] == 'Correy':
             Correy = Correy+1
-        elif row[2] == "Li"
+        elif row[2] == 'Li':
             Li = Li+1
-        elif row[2] == "O'Tooley"
+        elif row[2] == "O'Tooley":
             O_Tooley = O_Tooley+1
+Percent_Khan = round((Khan / Total_Votes)*100 ,0)
+Percent_Correy = round((Correy / Total_Votes)*100,0)
+Percent_Li = round((Li / Total_Votes)*100,0)
+Percent_O_Tooley = round((O_Tooley / Total_Votes)*100,0)
 
 print("Election Results")
 print("---------------------------------")
 print("Total Votes:" +str(Total_Votes))
 print("---------------------------------")
-print(Khan)
+print("Khan:"  + str(Percent_Khan) + "%"+ "(" + str(Khan) + ")")
+print("Correy:"  + str(Percent_Correy) + "%"+ "(" + str(Khan) + ")")
+print("Li:"  + str(Percent_Li) + "%"+ "(" + str(Khan) + ")")
+print("O_Tooley:"  + str(Percent_O_Tooley) + "%"+ "(" + str(Khan) + ")")
