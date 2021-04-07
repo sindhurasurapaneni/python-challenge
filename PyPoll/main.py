a6e6f8cc058c8a763a8c15f23bf9b4c6c9169d01
sindhurasurapaneni <sindhura.kp114@gmail.com>
2021-04-06 23:22:03 -0400

    PyPoll Code update

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,19 +1,3 @@
-# import csv
-
-# Khan = []
-# Correy = []
-# Li = []
-# O_Tooley = []
-
-# with open('C:\\Users\\sindh\\Desktop\\GitLab\\python-challenge\\PyPoll\\Resources\\election_data.csv') as datafile:
-#     Poll_data = csv.reader(datafile,delimiter= ",")
-#     csv_header = next(datafile)
-#     data = list(datafile)
-#     row_count = len(data)
-#     print ("Total Votes:" + str(row_count))
-
-#     for row in datafile:
-#      print (datafile)
 
 
 
@@ -23,14 +7,27 @@ import csv
 election_csv = os.path.join( "Resources", "election_data.csv")
 
 data = []
+Khan = 0
+Correy = 0
+Li = 0
+O_Tooley = 0
 with open(election_csv, newline='') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
     for row in csvreader: 
         data.append((row[0]))
         Total_Votes = len(data)
-        
+
+        if row[2] == 'Khan':
+            Khan = Khan+1
+        elif row[2] == 'Correy':
+            Correy = Correy+1
+        elif row[2] == "Li"
+            Li = Li+1
+        elif row[2] == "O'Tooley"
+            O_Tooley = O_Tooley+1
 
 print("Election Results")
 print("---------------------------------")
 print("Total Votes:" +str(Total_Votes))
 print("---------------------------------")
+print(Khan)
